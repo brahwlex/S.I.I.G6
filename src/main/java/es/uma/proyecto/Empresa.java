096d2bcece5aa02b10356ae79802a2b6f495e57e
sixto3 <antoniosixtolopezmalo@gmail.com>
2022-04-05 12:00:38 +0200

    Actualizado autorizado y empresa

--- src/main/java/es/uma/proyecto/Empresa.java
+++ src/main/java/es/uma/proyecto/Empresa.java
@@ -1,2 +1,27 @@
-package es.uma.proyecto;public class Empresa {
+package es.uma.proyecto;
+
+import java.util.Objects;
+
+import javax.persistence.*;
+
+public class Empresa extends Cliente{
+	@Column(nullable=false)
+	private String razon_social;
+
+	public Empresa(String razon_social) {
+		super();
+		this.razon_social = razon_social;
+	}
+
+	public String getRazon_social() {
+		return razon_social;
+	}
+
+	public void setRazon_social(String razon_social) {
+		this.razon_social = razon_social;
+	}
+
+	
+	
+	
 }
